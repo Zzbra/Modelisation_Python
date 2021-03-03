fac775c1d0fe74d0990b702e6abd4549a5ebd841
Etienne <roux.etienne141@gmail.com>
2021-03-03 01:48:21 +0100

    plot automation

--- main.py
+++ main.py
@@ -14,30 +14,41 @@ n_tab.append(n5)
 
 first_part = 'python3 schur_csp.py '
 second_part = ' -solver=[choco,v] | python3 test.py'
-for k in range(3, 6):
+for k in range(4, 5):
+    k_offset = k - 4
     time_val.append([])
     sat.append([])
-    for n in n_tab[k - 3]:
+    for n in n_tab[k_offset]:
         command = first_part + str(k) + " " + str(n) + second_part
         start = time.time()
         stream = os.popen(command)
         output = stream.read()
         end = time.time()
-        time_val[k - 3].append(end - start)
+        time_val[k_offset].append(end - start)
         if len(output) > 2:
-            sat[k - 3].append("SAT")
+            sat[k_offset].append("SAT")
         else:
-            sat[k - 3].append("UNSAT")
+            sat[k_offset].append("UNSAT")
 
-    # print(time_val)
-    # print(sat)
-    # print(n_tab)
-    plt.plot(n_tab[k-3], time_val[k-3])
-    plt.scatter(n_tab[k-3], time_val[k-3])
-    for i in range(len(n_tab[k-3])):
-        plt.annotate(sat[k-3][i] + " n: " + str(n_tab[k-3][i]), (n_tab[k-3][i], time_val[k-3][i] * 1.3 + 0.6))
-    plt.xlabel("Nombre de boules")
-    plt.ylabel("Temps de résolution")
-    plt.title("Temps de résolution en fonction du nombre de boules pour " + str(k) + " paniers")
-    plt.show()
+    # plt.plot(n_tab[k_offset], time_val[k_offset])
+    # plt.scatter(n_tab[k - 3], time_val[k - 3])
+    # for i in range(len(n_tab[k - 3])):
+    #     plt.annotate(sat[k - 3][i] + " n: " + str(n_tab[k - 3][i]), (n_tab[k - 3][i], time_val[k - 3][i] * 1.3 + 0.6))
+    # plt.xlabel("Nombre de boules")
+    # plt.ylabel("Temps de résolution")
+    # plt.title("Temps de résolution en fonction du nombre de boules pour " + str(k) + " paniers")
+    # plt.show()
 
+
+ind = 0
+print(time_val)
+print(sat)
+print(n_tab)
+plt.plot(n_tab[ind], time_val[ind])
+plt.scatter(n_tab[ind], time_val[ind])
+for i in range(len(n_tab[ind])):
+    plt.annotate(sat[ind][i] + " n: " + str(n_tab[ind][i]), (n_tab[ind][i], time_val[ind][i]*1.3+0.6))
+plt.xlabel("Nombre de boules")
+plt.ylabel("Temps de résolution")
+plt.title("Temps de résolution en fonction du nombre de boules pour trois paniers")
+plt.show()
